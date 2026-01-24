14b0e2624dd5aabb60d43a0a9c3e61f76cb9a78d
Georgi Trendafilov <goshost8@gmail.com>
2026-01-24 16:56:56 +0200

    Add project files.

---
+++ StoreManagementSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace StoreManagementSystem.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
