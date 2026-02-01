5d212b3365cb3003fa4523bcd3dfd2034c7a8bf1
Georgi Trendafilov <goshost8@gmail.com>
2026-02-01 20:23:14 +0200

    Implement Listing of all Products

---
+++ StoreManagementSystem/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿namespace StoreManagementSystem.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Data;
+    using Models;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ProductController : Controller
+    {
+        private readonly StoreDbContext dbContext;
+        public ProductController(StoreDbContext context)
+        {
+            dbContext = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> products = dbContext
+                .Products
+                .AsNoTracking()
+                .Include(p => p.Category)
+                .Include(p => p.Supplier)
+                .ToList();
+
+            return View(products);
+        }
+    }
+}
