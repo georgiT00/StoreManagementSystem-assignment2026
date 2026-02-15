c5af612690dcc77bb2b570b8ac2346b1496f6af0
Georgi Trendafilov <goshost8@gmail.com>
2026-02-15 23:07:37 +0200

    Add Order Service

---
+++ StoreManagementSystem.Services.Core/Interfaces/IOrderService.cs
@@ -0,0 +1,9 @@
+﻿namespace StoreManagementSystem.Services.Core.Interfaces
+{
+    using ViewModels.Order;
+    public interface IOrderService
+    {
+        Task <IEnumerable<OrderDetailsViewModel>> GetOrdersForUserAsync(string userId);
+    
+    }
+}
