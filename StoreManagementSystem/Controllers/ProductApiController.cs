66c56a51664ac7a3c63b34c4102b85b8d335bc6d
Georgi Trendafilov <goshost8@gmail.com>
2026-04-01 22:55:08 +0300

    Implement Showing Product Details using AJAX

---
+++ StoreManagementSystem/Controllers/ProductApiController.cs
@@ -0,0 +1,34 @@
+﻿namespace StoreManagementSystem.Controllers
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    using Services.Core.Interfaces;
+    using ViewModels.Product;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductApiController : ControllerBase
+    {
+        private readonly IProductService productService;
+
+        public ProductApiController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetProductDetails(int id)
+        {
+            ProductDetailsViewModel? viewModel = await productService.GetProductDetailsByIdAsync(id);
+
+            if (viewModel == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(viewModel);
+        }
+
+    }
+}
---
+++ StoreManagementSystem/wwwroot/js/product.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    $("#product-details").on("click", function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        const id = $(this).attr("data-product-id");
+
+        $.ajax({
+            url: `/api/ProductApi/${id}`,
+            type: 'GET',
+            success: function (data) {
+                $('#productDetailsModalLabel').text(data.productName);
+                $('#productCategory').text(data.categoryName);
+                $('#productSupplier').text(data.supplierName || 'N/A');
+                $('#productQuantity').text(data.quantity);
+                $('#productPrice').text(`$${data.price}`);
+
+                $('#editProductLink').attr('href', `/Product/Edit/${data.productId}`);
+                $('#deleteProductLink').attr('href', `/Product/Delete/${data.productId}`);
+
+                $('#productDetailsModal').modal('show');
+            },
+            error: function () {
+                alert('Failed to fetch product details. Please try again.');
+            }
+        });
+    });
+});
